8f25ea68aed85d5f59d43bd67a8ddaa2b5bc4b6b
Gabriel Oprisan <gabriel.oprisan@gcore.com>
2023-11-13 17:25:49 +0200

    Add http request body handler support

--- nginx_module/src/lib.rs
+++ nginx_module/src/lib.rs
@@ -6,18 +6,19 @@ mod bindings;
 use std::{
     ffi::{CStr, CString},
     str::FromStr,
+    sync::Mutex,
 };
 
 pub use bindings::{
-    nginx_version, ngx_command_t, ngx_conf_t, ngx_cycle_t, ngx_http_conf_ctx_t, ngx_http_module_t,
-    ngx_http_request_t, ngx_module_t, ngx_str_t, NGX_CONF_TAKE1, NGX_DECLINED, NGX_ERROR,
-    NGX_HTTP_FORBIDDEN, NGX_HTTP_LOC_CONF, NGX_HTTP_MAIN_CONF, NGX_HTTP_MODULE, NGX_HTTP_SRV_CONF,
-    NGX_HTTP_TEMPORARY_REDIRECT, NGX_LOG_ERR, NGX_OK, NGX_RS_HTTP_LOC_CONF_OFFSET,
-    NGX_RS_MODULE_SIGNATURE,
+    nginx_version, ngx_chain_t, ngx_command_t, ngx_conf_t, ngx_cycle_t, ngx_http_conf_ctx_t,
+    ngx_http_module_t, ngx_http_request_body_filter_pt, ngx_http_request_t, ngx_module_t,
+    ngx_str_t, NGX_CONF_TAKE1, NGX_DECLINED, NGX_ERROR, NGX_HTTP_FORBIDDEN, NGX_HTTP_LOC_CONF,
+    NGX_HTTP_MAIN_CONF, NGX_HTTP_MODULE, NGX_HTTP_SRV_CONF, NGX_HTTP_TEMPORARY_REDIRECT,
+    NGX_LOG_ERR, NGX_OK, NGX_RS_HTTP_LOC_CONF_OFFSET, NGX_RS_MODULE_SIGNATURE,
 };
 use bindings::{
     ngx_array_push, ngx_cycle, ngx_http_core_main_conf_t, ngx_http_core_module,
-    ngx_http_handler_pt, ngx_http_phases_NGX_HTTP_ACCESS_PHASE,
+    ngx_http_handler_pt, ngx_http_phases_NGX_HTTP_ACCESS_PHASE, ngx_http_top_request_body_filter,
 };
 
 mod ngx_str;
@@ -26,7 +27,6 @@ pub use ngx_str::NgxStr;
 
 mod complex_value;
 pub use complex_value::ComplexValue;
-
 
 mod http_request;
 pub use http_request::{HttpRequest, HttpRequestAndContext};
@@ -148,6 +148,14 @@ pub trait HttpHandler<'a>: Sized {
     fn handle(req: &mut HttpRequestAndContext<'a, Self>) -> anyhow::Result<isize>;
 }
 
+pub trait HttpRequestBodyHandler<'a>: Sized {
+    fn next_handler() -> &'a Mutex<ngx_http_request_body_filter_pt>;
+    fn handle(
+        req: &mut HttpRequestAndContext<'a, Self>,
+        buf: *mut ngx_chain_t,
+    ) -> anyhow::Result<isize>;
+}
+
 ///
 /// # Safety
 ///  
@@ -192,5 +200,50 @@ unsafe extern "C" fn ngx_http_generic_handler<'a, H: HttpHandler<'a> + Default +
             }
             NGX_ERROR as isize
         }
+    }
+}
+
+pub fn add_request_body_handler<'a, H: HttpRequestBodyHandler<'a> + Default + 'a>() {
+    unsafe {
+        if let Ok(mut handler) = H::next_handler().lock() {
+            *handler = ngx_http_top_request_body_filter
+        }
+        ngx_http_top_request_body_filter = Some(body_handler::<'a, H>);
+    }
+}
+
+unsafe extern "C" fn body_handler<'a, H: HttpRequestBodyHandler<'a> + Default + 'a>(
+    req: *mut ngx_http_request_t,
+    buf: *mut ngx_chain_t,
+) -> isize {
+    if let Some(req) = HttpRequestAndContext::from_raw(req) {
+        match H::handle(req, buf) {
+            Ok(result) => {
+                if result != NGX_OK as isize {
+                    return result;
+                }
+            }
+            Err(e) => {
+                if let Ok(err_msg) = CString::new(e.to_string()) {
+                    ngx_log_error(NGX_LOG_ERR as usize, (*ngx_cycle).log, 0, &err_msg);
+                }
+                return NGX_ERROR as isize;
+            }
+        }
+    }
+    if let Ok(next) = H::next_handler().lock() {
+        if let Some(next) = *next {
+            next(req, buf)
+        } else {
+            NGX_OK as isize
+        }
+    } else {
+        ngx_log_error(
+            NGX_LOG_ERR as usize,
+            (*ngx_cycle).log,
+            0,
+            CStr::from_bytes_with_nul_unchecked(b"Poisoned lock in request body handler\0"),
+        );
+        NGX_ERROR as isize
     }
 }
