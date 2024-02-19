8e1ba900f7479e093840afe3a60fe770e54402b5
Gabriel Oprisan <gabriel.oprisan@gcore.com>
2024-02-19 21:55:14 +0200

    Expose ngx_current_msec

--- nginx_module/src/lib.rs
+++ nginx_module/src/lib.rs
@@ -16,10 +16,10 @@ pub use bindings::{
     ngx_module_t, ngx_str_t, NGX_AGAIN, NGX_CONF_TAKE1, NGX_CONF_TAKE2, NGX_DECLINED, NGX_ERROR,
     NGX_HTTP_FORBIDDEN, NGX_HTTP_LOC_CONF, NGX_HTTP_MAIN_CONF, NGX_HTTP_MODULE, NGX_HTTP_SRV_CONF,
     NGX_HTTP_TEMPORARY_REDIRECT, NGX_LOG_ERR, NGX_OK, NGX_RS_HTTP_LOC_CONF_OFFSET,
-    NGX_RS_MODULE_SIGNATURE,
+    NGX_RS_MODULE_SIGNATURE, ngx_current_msec
 };
 use bindings::{
-    ngx_array_push, ngx_current_msec, ngx_cycle, ngx_event_t, ngx_event_timer_rbtree,
+    ngx_array_push, ngx_cycle, ngx_event_t, ngx_event_timer_rbtree,
     ngx_http_core_main_conf_t, ngx_http_core_module, ngx_http_handler_pt,
     ngx_http_phases_NGX_HTTP_ACCESS_PHASE, ngx_http_top_request_body_filter, ngx_queue_t,
     ngx_rbtree_delete, ngx_rbtree_insert,
